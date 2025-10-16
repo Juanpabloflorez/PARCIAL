ee9a2cb117961153beea99f4386a55ab7d0e6dc4
Juanpabloflorez <juflorezru@unal.edu.co>
2025-10-16 15:22:56 -0500

    Update parcial.py

--- parcial.py
+++ parcial.py
@@ -1,30 +1,47 @@
-from usuario import Usuarios
+from user import Usuarios
 from libros import Libros
+import firebase_admin
+from firebase_admin import db,credentials
 
-def nain():
-    print("Bienvenido a la Biblioteca Universitaria")
-    opc=int(input("Ingrese 1 para registrar usuario, ingrese 2 para registrar libros: "))
+cred=credentials.Certificate("credentials.json")
+firebase_admin.initialize_app(cred, {"databaseURL": "https://trabajo-opcional-puntos-extra-default-rtdb.firebaseio.com/"})
 
-    if (opc==1):
+ref = db.reference("/")
+
+def registrarUsuario():
         name=input("Ingrese su nombre: ")
         age=input("Ingrese su edad: ")
         ident=input("Cree su ID: ")
 
         Usuarios(nombre=name, edad=age, id=ident).registrar_usuario
+        ref.child("Usuarios").child(ident).set({"Nombre": name,"Edad": age})
+
         print(f"Usuario creado exitosamente, Nombre: ",name,", Edad: ",age,", ID creada: ",ident)
 
-    if (opc==2):
+   
+def registrarLibro():
         book=input("Ingrese el nombre del libro: ")
-        genrer=int(input("Ingrese el genero (1 para matematicas, 2 para ciencias y 3 para humanas): "))
+        genero=int(input("Ingrese el genero (1 para matematicas, 2 para ciencias y 3 para humanas): "))
+        bookID=input("Ingrese el codigo del libro: ")
 
-        if (genrer==1):
-            genrer="matematicas"
-        if (genrer==2):
-            genrer="ciencias"
-        if (genrer==3):
-            genrer="humanas"
+        while genero != 1 and genero != 2 and genero != 3:
+             genero = input("Asignacion de genero no valido, ingrese un valor valido del 1 al 3: ")
 
-        Libros(titulo=book, categoria=genrer).registrar_libro
-        print(f"Libro agregado exitosamente, Titulo: ",book,", Genero: ",genrer)
+        if (genero==1):
+            genero="matematicas"
+        if (genero==2):
+            genero="ciencias"
+        if (genero==3):
+            genero="humanas"
+          
+        Libros(titulo=book, categoria=genero).registrar_libro
+        ref.child("Libros").child(bookID).set({"Nombre": book,"Categoria":genero})
 
-nain()
+        print(f"Libro agregado exitosamente, Titulo: ",book,", Categoria: ",genero)
+
+
+def main():
+    registrarLibro()
+
+if __name__ == "__main__":
+    main()
